aba5ffb4c9f74b8d99ddf28671cc76e913af5451
Damien Robson <damien.p.robson@gmail.com>
2025-09-19 16:28:41 +0100

    Add sitemap package

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,15 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
 
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("manifest.json");
+
+  eleventyConfig.addPassthroughCopy("_headers");
+  eleventyConfig.addPassthroughCopy("_redirects");
+
+  eleventyConfig.addPassthroughCopy("icons");
+  eleventyConfig.addPassthroughCopy("images");
+
   eleventyConfig.addWatchTarget("./src/css/");
 
   return {
